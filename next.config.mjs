b5c60a00a24bfcbdf52a2f7cca3c3a65c203fcf7
Israel Ramos <arnulfo_israel@hotmail.com>
2024-07-07 18:18:43 -0600

    chore(ghpages) deploy to pages with actions, checking if i need out folder on the project

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  basePath: "/my-next",
-  output: "export", // <=== enables static exports
-  reactStrictMode: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
